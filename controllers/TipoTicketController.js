06cf5c0c5d7b51be327fc3149874a478150b9e5c
RollyCZ <rcayllahua@autonoma.edu.pe>
2020-08-20 22:04:02 -0500

    Rolly
    Controlador tipoticket

--- controllers/TipoTicketController.js
+++ controllers/TipoTicketController.js
@@ -1,58 +1,66 @@
-import models from '../models';
-export default {
-    add: async (req,res,next) =>{
-        try {
-            const reg = await models.Ticket.create(req.body);
+import models from '../models'; 
+ //se declara los metodos para la conexion con mongo db
+
+ export default {
+    add: async (req, res, next) => {
+        try { 
+            const reg = await models.Tipoticket.create(req.body);
             res.status(200).json(reg);
-        } catch (e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrio un error'
             });
             next(e);
         }
     },
-    query: async (req,res,next) => {
+    query: async (req, res, next) => {
         try {
-            const reg=await models.Ticket.findOne({_id:req.query._id})
-            .populate('usuario',{usuario:3})
-			.populate('tipoticket',{nombre:1})
-			.populate('equipo',{tipo:1});
-            if (!reg){
+            const reg = await models.Tipoticket.findOne({ _id: req.query._id })
+            if (!reg) {
                 res.status(404).send({
                     message: 'El registro no existe'
-                });
-            } else{
+                })
+            } else {
                 res.status(200).json(reg);
             }
-        } catch(e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrio un error'
             });
             next(e);
         }
     },
-    //faltan estas
-	list: async (req,res,next) => {
+    list: async (req, res, next) => {
         try {
-            let valor=req.query.valor;
-            const reg=await models.Articulo.find({$or:[{'nombre':new RegExp(valor,'i')},{'descripcion':new RegExp(valor,'i')}]},{createdAt:0})
-            .populate('categoria',{nombre:1})
-            .sort({'createdAt':-1});
+            let valor = req.query.valor;
+            const reg = await models.Tipoticket.find({ $or: [{ 'nombre': new RegExp(valor, 'i') }, { 'descripcion': new RegExp(valor, 'i') }] }, { createAt: 0 })
+                .sort({ 'createAt': -1 });
             res.status(200).json(reg);
-        } catch(e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrio un error'
             });
             next(e);
         }
     },
-    update: async (req,res,next) => {
-        try {
-            const reg = await models.Articulo.findByIdAndUpdate({_id:req.body._id},{categoria:req.body.categoria,codigo:req.body.codigo,nombre:req.body.nombre,descripcion:req.body.descripcion,precio_venta:req.body.precio_venta,stock:req.body.stock});
+    update: async (req, res, next) => {
+        try { 
+            const reg = await models.Tipoticket.findByIdAndUpdate({ _id: req.body._id }, { descripcion: req.body.descripcion, nombre: req.body.nombre});
             res.status(200).json(reg);
-        } catch(e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
+    remove: async (req, res, next) => {
+        try {
+            const reg = await models.Tipoticket.findByIdAndDelete({ _id: req.query._id });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
             });
             next(e);
         }
